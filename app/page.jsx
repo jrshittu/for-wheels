a81aec52debf4cb45cfb6748c8820674f867bea3
Abideen <misterjrshittu@gmail.com>
2023-09-09 14:17:44 +0100

    Update Nav

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <section className="w-full flex-center flex-col">
+      
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden'/>
+    </h1>
+
+    <h1 className='head_text text-center orange_gradient'>AI-Powered Prompts</h1>
+
+    <p className='desc text-center'>
+      Promptus is an open source AI prompting tool for modern world to discover, create and share creative prompts.
+    </p>
+
+    <Biometric />
+  </section>
   )
 }
 
