5166e996749c86dcb0b75106903044cb263681b7
Abideen <misterjrshittu@gmail.com>
2023-09-09 15:08:52 +0100

    Update public files

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
 
-const page = () => {
+const Page = () => {
   return (
     <section className="w-full flex-center flex-col">
+
+        <div className='sm:flex hidden'>
+            <Image 
+                src="/assets/images/park.png"
+                width={180}
+                height={100}
+                className='rounded_full'
+                alt="profile"
+            />
+        </div>
       
-    <h1 className='head_text text-center'>
-      Discover & Share
-      <br className='max-md:hidden'/>
-    </h1>
+        <h1 className='head_text text-center'>
+            Discover ParkEase
+        <br className='max-md:hidden'/>
+        </h1>
 
-    <h1 className='head_text text-center orange_gradient'>AI-Powered Prompts</h1>
+        <h1 className='head_text text-center orange_gradient'>Streamlining Parking Solutions</h1>
 
-    <p className='desc text-center'>
-      Promptus is an open source AI prompting tool for modern world to discover, create and share creative prompts.
-    </p>
+        <p className='desc text-center'>
+        ParkEase is an innovative and user-centric application designed to
+            transform the parking experience. With cutting-edge features, ParkEase
+            redefines convenience and efficiency in car parking management.
+        </p>
 
-    <Biometric />
   </section>
   )
 }
 
-export default page
+export default Page
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,83 @@
+"use client";
+
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
 
 const Nav = () => {
+    const isUserLoggedIn = true;
+
+    const [ providers, setProviders ] = useState(null);
+    
+    useEffect(() => {
+        const setUpProviders = async () => {
+    
+          const response = await getProviders();
+    
+          setProviders(response);
+    
+        }
+        setUpProviders();
+      }, []);
+
   return (
-    <div>
-      Nav
-    </div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'> 
+            <Image 
+                src="/assets/images/logo.png"
+                alt='Car Parking Solution'
+                width={70}
+                height={65}
+                className='object-contain'
+            />
+            <p className='logo_text'>
+                ParkEase
+            </p>
+        </Link>
+
+        {/* Desktop Nav */}
+        <div className='sm:flex hidden'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/register-car" className='black_btn'>
+                        Register Your Car
+                    </Link>
+                    <button type="button" onClick={signOut} className='outline_btn'> 
+                        Sign Out
+                    </button>
+                    <Link href="/profile">
+                        <Image 
+                            src="/assets/images/logo.png"
+                            width={37}
+                            height={37}
+                            className='rounded_full'
+                            alt="profile"
+                        />
+                    </Link>
+                </div>
+            ) : (
+                <>
+                    { providers && 
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={providers.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              )) 
+            }
+          </>
+        )
+      }
+      </div>
+      
+    </nav>
   )
 }
 
