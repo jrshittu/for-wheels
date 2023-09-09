93a66723b91bab9b35aa50d3d8758735c7e90975
Abideen <misterjrshittu@gmail.com>
2023-09-09 18:09:16 +0100

    add auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,50 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-import { connectToDB } from "@utils/database";
+import { connectToDB } from '@utils/database';
 import User from '@models/user';
 
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.Client_ID,
+            clientSecret: process.env.Client_secret,
+        })
+    ],
+    async session({ session }){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        });
+
+        session.user.id = sessionUser._id.toString();
+
+        return session;
+
+    },
+    async signIn({ profile }){
+        try {
+            await connectToDB();
+
+            // check if user exists
+            const userExists = await User.findOne({
+                email: profile.mail
+            });
+
+            // if does'nt exists, create a new user
+            if(!userExists){
+                await User.create({
+                     email: profile.email,
+                     username: profile.name.replace(" ", "").toLowerCase(),
+                     image: profile.picture
+                });
+            }
+
+            return true;
+        } catch (error) {
+            console.log(error);
+
+            return false;
+        }
+    }
+})
+
+export { handler as GET, handler as POST };
