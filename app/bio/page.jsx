60e9e1c281135977174101fe0b13b7d1c326afd1
Abideen <misterjrshittu@gmail.com>
2023-09-10 13:20:50 +0100

    Update page

--- app/bio/page.jsx
+++ app/bio/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
     <div>
       Hello Bio
@@ -8,4 +8,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
