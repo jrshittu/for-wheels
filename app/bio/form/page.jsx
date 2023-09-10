e9b666461063b4a014ecc6d6c830fb4e38304339
Abideen <misterjrshittu@gmail.com>
2023-09-10 17:07:22 +0100

    update form

--- app/bio/form/page.jsx
+++ app/bio/form/page.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
 
 const Page = () => {
+    const [ submitting, setSubmitting ] = useState(false);
+    const [ post, setPost ] = useState([
+          {id: 1,
+            question: 'How satisfied are you with our service?',
+            type: 'rating',
+            response: null,
+          },
+          {
+            id: 2,
+            question: 'What can we improve to serve you better?',
+            type: 'text',
+            response: '',
+          },
+          {
+            id: 3,
+            question: 'Would you recommend our service to others?',
+            type: 'rating',
+            response: null,
+          },
+          {
+            id: 4,
+            question: 'How often do you use our service?',
+            type: 'multiple-choice',
+            response: '',
+            choices: ['Daily', 'Weekly', 'Monthly', 'Rarely', 'Never'],
+          }
+    ]);
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const createQuestion = async (e) => {
+
+    }
+
   return (
-    <div>
-      Form Page
-    </div>
+    <Form 
+        type="Form"
+        post={post}
+        setPost= {setPost}
+        submitting={submitting}
+        handleSubmit = {createQuestion}
+    />
   )
 }
 
-export default Page
+export default Page;
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>
-      Form
-    </div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='orange_gradient'>{type}</span>
+      </h1>
+
+    </section>
   )
 }
 
