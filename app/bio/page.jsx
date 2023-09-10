f2b944aef7221d14999f70a95a1b45fa25cb1e14
Abideen <misterjrshittu@gmail.com>
2023-09-10 21:46:04 +0100

    added scan page

--- app/bio/page.jsx
+++ app/bio/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QRCodeGenerator from './QRCodeGenerator';
+import QRCodeScanner from './QRCodeScanner';
 
 const Page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [mode, setMode] = useState('generate'); // 'generate' or 'scan'
+  const [qrData, setQRData] = useState(''); // Data for QR code generation
 
-export default Page
+  const toggleMode = () => {
+    setMode(mode === 'generate' ? 'scan' : 'generate');
+    setQRData(''); // Clear QR code data when switching modes
+  };
+
+  return (
+    <div className="app">
+      <h1>QR Code Generator and Scanner</h1>
+      <button onClick={toggleMode}>
+        {mode === 'generate' ? 'Switch to Scan Mode' : 'Switch to Generate Mode'}
+      </button>
+      {mode === 'generate' ? (
+        <QRCodeGenerator data={qrData} />
+      ) : (
+        <QRCodeScanner />
+      )}
+    </div>
+  );
+};
+
+export default Page;
