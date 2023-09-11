87dbc4f6c094bfaee95da7583cefba30ee87172c
Abideen <misterjrshittu@gmail.com>
2023-09-11 10:37:26 +0100

    Update Page

--- app/page.jsx
+++ app/page.jsx
@@ -16,26 +16,23 @@ const Page = () => {
             />
         </div>
 
-        
-      
         <h1 className='head_text text-center green_gradient mb-2'>
             Discover ParkEase
         </h1>
+
         <div className='flex flex-column mt-3'>
             <Link href="/bio/form" className='black_btn mx-2'>
                             Open Questionnaire
             </Link>
             <Link href="/bio" className='black_btn mx-2'>
-                            Scan Qr Code
+                            Scan QR Code
             </Link>
         </div>
+
         <h1 className='orange_gradient head_text text-center mt-2'>Streamlining Parking Solutions</h1>
         
-
         <p className='desc text-center'>
-        ParkEase is an innovative app designed to
-            transform your parking experience. With cutting-edge features, ParkEase
-            redefines convenience and efficiency in car parking management. Kindly attend to questionnaire below to help us improve our services.
+            ParkEase is an innovative app designed to transform your parking experience. With cutting-edge features, ParkEase redefines convenience and efficiency in car parking management. Kindly attend to questionnaire below to help us improve our services.
         </p>
 
   </section>
