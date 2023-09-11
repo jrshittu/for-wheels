a151e5358faa483198ce450ca3b1bb66deb6295e
Abideen <misterjrshittu@gmail.com>
2023-09-11 22:54:15 +0100

    Uodate GeoLocation Component

--- components/GeoLocation.jsx
+++ components/GeoLocation.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from 'react';
 
-const GeoLocation = () => {
+const GeolocationComponent = ({ onLocationChange }) => {
+  const [userLocation, setUserLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+          onLocationChange({ latitude, longitude }); // Pass location to parent component
+        },
+        (geoError) => {
+          setError(geoError.message);
+        }
+      );
+    } else {
+      setError('Geolocation is not supported in your browser.');
+    }
+  }, []);
+
   return (
     <div>
-      Geolocation
+      {userLocation ? (
+        <p>
+          Your Location: Lat {userLocation.latitude}, Long{' '}
+          {userLocation.longitude}
+        </p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <p>Fetching your location...</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GeoLocation
+export default GeolocationComponent;
