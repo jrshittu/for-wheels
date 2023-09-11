6fc85c2cc62809977516510db5a944423d3a31b3
Abideen <misterjrshittu@gmail.com>
2023-09-11 23:03:51 +0100

    update page.jsx

--- app/track/page.jsx
+++ app/track/page.jsx
@@ -1,11 +1,35 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import ParkingData from '@components/ParkingData';
+import Head from 'next/head';
+import GeolocationComponent from '../components/GeolocationComponent';
+import MappingComponent from '../components/MappingComponent';
 
 const Page = () => {
+
+  const [userLocation, setUserLocation] = useState(null);
+  const [parkingSpaces, setParkingSpaces] = useState([]); // You will fetch this data from your API
+
+  const handleLocationChange = (location) => {
+    setUserLocation(location);
+    // You can also fetch parking space data here using the user's location
+    // For simplicity, we assume you already have parking space data available
+  };
+
   return (
     <div>
-      <ParkingData />
+
+      <Head>
+        <title>Parking Locator</title>
+      </Head>
+
+      <h1>Parking Locator</h1>
+      <GeolocationComponent onLocationChange={handleLocationChange} />
+      {userLocation && (
+        <MappingComponent userLocation={userLocation} parkingSpaces={parkingSpaces} />
+      )}
+
+      <ParkingData />npm
     </div>
   )
 }
