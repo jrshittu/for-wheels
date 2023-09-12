38933e609a37930062ac3fe49b9dc2ce42b63955
Abideen <misterjrshittu@gmail.com>
2023-09-12 12:01:20 +0100

    Update global.css and page

--- app/track/page.jsx
+++ app/track/page.jsx
@@ -11,19 +11,17 @@ const Page = () => {
 
   const handleLocationChange = (location) => {
     setUserLocation(location);
-    // You can also fetch parking space data here using the user's location
-    // For simplicity, we assume you already have parking space data available
   };
 
   return (
     <div>
-      
-      <h1>Parking Locator</h1>
+      <h1 className="head_text green_gradient mb-4">Parking Locator</h1>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d126834.05801193288!2d3.3292288!3d6.5765376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1694515327364!5m2!1sen!2sng" className="h-450 w-600 b-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <GeolocationComponent onLocationChange={handleLocationChange} />
-      {userLocation !== null ? (
+      {userLocation == null ? (
         <MappingComponet userLocation={userLocation} parkingSpaces={parkingSpaces} />
       ) : (
-        <p>Loading user location...</p>
+        <p>Check Available Parking Space</p>
       )}
 
       <ParkingData />
